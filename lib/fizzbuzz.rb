d346bfb16e7ea1062bc2e5c88907391310035b64
jpates1 <jamesandrewpates@gmail.com>
2023-01-31 13:59:08 +0000

    final commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz(number)
-  'fizz'
-end
+def fizzbuzz(range)
+  range.map do |num|
+  if num % 3 == 0 && num % 5 == 0
+  "FizzBuzz"
+  elsif num % 3 == 0
+  "Fizz"
+  elsif num % 5 == 0
+  "Buzz"
+  else
+  num
+  end
+  end
+  end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
+describe 'Fizz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+end
+
+describe 'Buzz' do
+  it 'returns "Buzz" when passed 3' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "FizzBuzz" when passed 3' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "2" when passed anything but 3, 5, 15' do
+    expect(fizzbuzz(2)).to eq "#{2}"
   end
 end
